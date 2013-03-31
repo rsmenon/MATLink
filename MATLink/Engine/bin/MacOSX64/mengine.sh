4cc1f04a6b9b4ad314cdf210c4671b582447c9fa
Szabolcs Horvát <szhorvat@gmail.com>
2013-03-31 14:57:58 -0400

    Fix sample MATLAB path name in OS X launch script

--- MATLink/Engine/bin/MacOSX64/mengine.sh
+++ MATLink/Engine/bin/MacOSX64/mengine.sh
@@ -2,7 +2,7 @@
 
 # Before using mEngine, you need to
 # set the path to your MATLAB installation here:
-# MATLAB=/Applications/MATLAB_2013a.app
+# MATLAB=/Applications/MATLAB_R2012b.app
 MATLAB=`ls -d /Applications/MATLAB*.app | tail -n 1`
 
 export DYLD_LIBRARY_PATH=$MATLAB/bin/maci64:$DYLD_LIBRARY_PATH
