b2fb71fe6d15dc380ca41f6b4976dbefef9748d0
R. Menon <rsmenon@icloud.com>
2013-04-06 08:26:17 -0700

    define new functions inline

--- MATLink/MATLink.m
+++ MATLink/MATLink.m
@@ -440,11 +440,12 @@ MScript /: DeleteFile[MScript[name_String]] :=
 		$error
 	]
 
-Options[MFunction] = {"Output" -> True, "OutputArguments" -> 1};
-validOptionPatterns[MFunction] = {"Output" -> True | False, "OutputArguments" -> _Integer?Positive};
+Options[MFunction] = {"NewFunction" -> False, "Output" -> True, "OutputArguments" -> 1};
+validOptionPatterns[MFunction] = {"NewFunction" -> True | False, "Output" -> True | False, "OutputArguments" -> _Integer?Positive};
 (* Since MATLAB allows arbitrary function definitions depending on the number of output arguments, we force the user to explicitly specify the number of outputs if it is different from the default value of 1. *)
 
 MFunction::args = "The arguments at positions `1` to \"`2`\" could not be translated to MATLAB."
+MFunction::nfun = "To define a new function, use the \"NewFunction\" \[Rule] True option."
 
 MFunction[name_String, opts : OptionsPattern[]][args___] /; MATLABInstalledQ[] && validOptionsQ[MFunction, {opts}] :=
 	Switch[OptionValue["Output"],
@@ -473,6 +474,13 @@ MFunction[name_String, opts : OptionsPattern[]][args___] /; MATLABInstalledQ[] &
 			iMEvaluate[StringJoin["clear ", Riffle[vars, " "]], "NoCheck"];
 		]
 	] /; engineOpenQ[]
+
+MFunction[name_String, code_String, opts : OptionsPattern[]] /; MATLABInstalledQ[] && validOptionsQ[MFunction, {opts}] :=
+	If[OptionValue["NewFunction"] === True,
+		MScript[name, code, "Overwrite" -> True];
+		MFunction[name, Sequence @@ FilterRules[{opts}, Except["NewFunction"]]],
+		message[MFunction::nfun]["error"]
+	]
 
 MFunction[name_String, OptionsPattern[]][args___] /; MATLABInstalledQ[] := message[MFunction::wspc]["warning"] /; !engineOpenQ[]
 MFunction[name_String, OptionsPattern[]][args___] /; !MATLABInstalledQ[] := message[MFunction::engc]["warning"]
