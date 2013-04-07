6244b64110df936a07e3af77d82d12c9cd1fcd7a
Szabolcs Horvát <szhorvat@gmail.com>
2013-04-07 14:11:38 -0400

    Commented start mengine.sh scripts and gave more explicit instructions for OS X

--- MATLink/Engine/bin/Unix64/mengine.sh
+++ MATLink/Engine/bin/Unix64/mengine.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Linux 64 bit MATLink start script
+# Modify the variables below to use a custom MATLAB installation:
+
 MATLAB=$(dirname $(readlink -f $(which matlab)))/..
 MATHLINK=$(dirname $(readlink -f $(which math)))/../SystemFiles/Links/MathLink/DeveloperKit/Linux-x86-64/CompilerAdditions
 
