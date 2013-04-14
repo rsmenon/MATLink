f2f62750e09a77af865812ec00aea073242c251b
Szabolcs Horvát <szhorvat@gmail.com>
2013-04-14 16:28:33 -0400

    Fixes #33

--- MATLink/MATLink.m
+++ MATLink/MATLink.m
@@ -709,10 +709,12 @@ convertToMATLAB[expr_] :=
 				engMakeCell[reshape@arr /. handle -> Identity, Reverse@Dimensions[arr]];
 
 			(* http://mathematica.stackexchange.com/questions/18081/how-to-interpret-the-fullform-of-a-sparsearray *)
-			MSparseArray[HoldPattern@SparseArray[Automatic, {n_, m_}, def_ /; def==0, {1, {jc_, ir_}, values_}]] :=
-				If[ complexArrayQ[values],
-					engMakeSparseComplex[Flatten[ir]-1, jc, Re[values], Im[values], m, n],
-					engMakeSparseReal[Flatten[ir]-1, jc, values, m, n]
+			MSparseArray[HoldPattern@SparseArray[Automatic, {n_, m_}, def_ /; def==0, {1, {jc_, ir_}, val_}]] :=
+				With[{values=Developer`ToPackedArray@N[val]},
+					If[ complexArrayQ[values],
+						engMakeSparseComplex[Flatten[ir]-1, jc, Re[values], Im[values], m, n],
+						engMakeSparseReal[Flatten[ir]-1, jc, values, m, n]
+					]
 				];
 			MSparseArray[_] := (message[MSet::spdef]["error"]; $Failed);
 
