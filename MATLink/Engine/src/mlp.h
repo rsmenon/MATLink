3bcb3a4948308a53f36ccaab1647987eaaeac617
Szabolcs Horvát <szhorvat@gmail.com>
2013-04-15 10:00:17 -0400

    Preliminary SparseArray fix for Win64/MSVC. Fixes #39

--- MATLink/Engine/src/mlp.h
+++ MATLink/Engine/src/mlp.h
@@ -5,6 +5,8 @@
  *
  */
 
+#ifndef MLP_H
+#define MLP_H
 
 #include <mathlink.h>
 
@@ -31,3 +33,17 @@ inline int mlpPutIntegerList(MLINK link, mlint64 *a, int n) {
 inline int mlpPutIntegerList(MLINK link, unsigned long *a, int n) {
     return MLPutInteger64List(link, (mlint64 *) a, n);
 }
+
+
+#ifdef _MSC_VER
+#if _WIN64
+
+inline int mlpPutIntegerList(MLINK link, size_t *a, int n) {
+    return MLPutInteger64List(link, (mlint64 *) a, n);
+}
+
+#endif
+#endif
+
+
+#endif MLP_H
