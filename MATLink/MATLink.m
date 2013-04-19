5e7d2bd687dfea05dad0943e9c1af880ca8a6e83
R. Menon <rsmenon@icloud.com>
2013-04-19 08:25:54 -0700

    catch uncaught throw

--- MATLink/MATLink.m
+++ MATLink/MATLink.m
@@ -263,12 +263,15 @@ validOptionsQ[func_Symbol, opts_List] :=
 	]
 
 checkExpression[expr_] :=
-	With[{invalid = Cases[MATLink`Engine`dispatcher[expr, "Throw" -> False], HoldPattern[$Failed -> x_] :> x, {0, Infinity}]},
-		If[invalid === {},
-			message[MATLink::noerr]["error"],
-			message[MATLink::errx, "The following sub-expressions are invalid: " <> ToString@invalid <> ". Check to see if they are supported by MATLink and/or if the dimensions are consistent"]["error"];
-		];
-		$Failed
+	Catch[
+		With[{invalid = Cases[MATLink`Engine`dispatcher[expr, "Throw" -> False], HoldPattern[$Failed -> x_] :> x, {0, Infinity}]},
+			If[invalid === {},
+				message[MATLink::noerr]["error"],
+				message[MATLink::errx, "The following sub-expressions are invalid: " <> ToString@invalid <> ". Check to see if they are supported by MATLink and/or if the dimensions are consistent"]["error"];
+			];
+			$Failed
+		],
+		$dispTag
 	]
 
 SetAttributes[switchAbort, HoldRest]
